11cb8840a46b7cabd9b88ef19352cded2a8d21e6
Sebastian Sandner <sebastian.sandner89@gmail.com>
2025-02-02 22:51:45 +0100

    display test.

---
+++ display/test_display.py
@@ -0,0 +1,17 @@
+from display.display import EInkDisplay
+from PIL import Image
+import os
+
+if __name__ == "__main__":
+    eInkDisplay = EInkDisplay(984, 1304)
+
+    # Load the images saved by render.py
+    directory = '/home/knallfrosch/personal/inkal/render'
+    red_image_path = os.path.join(directory, 'red_image.png')
+    black_image_path = os.path.join(directory, 'black_image.png')
+
+    red_image = Image.open(red_image_path)
+    black_image = Image.open(black_image_path)
+
+    # Display the images
+    eInkDisplay.display(black_image, red_image)
