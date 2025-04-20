9a97a9c092acc6e2b880ba4a1fb2ac964aa205ff
Sebastian Sandner <sebastian.sandner89@gmail.com>
2025-04-20 19:47:28 +0200

    less sleep time

--- shell/startup.sh
+++ shell/startup.sh
@@ -3,8 +3,8 @@
 # Navigate to the project directory
 cd /home/pi/inkal
 
-# Wait for 1 minute (60 seconds)
-sleep 60
+# Wait for 1 second
+sleep 1
 
 # Run the Python script
 poetry run python /home/pi/inkal/maginkcal.py
