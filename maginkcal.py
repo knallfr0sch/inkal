358896453f12eb0b5d383e60dc5b90f991ff42ce
Sebastian Sandner <sebastian.sandner89@gmail.com>
2026-01-04 16:48:30 +0100

    feat: Change to http

--- maginkcal.py
+++ maginkcal.py
@@ -10,73 +10,40 @@ CSS stylesheets in the "render" folder.
 """
 
 
-from typings_google_calendar_api.calendars import Calendar
-from config import Config
-from gcal.google_auth import GoogleAuth
-from gcal.google_calendar import GoogleCalendar
-from gcal.google_tasks import GoogleTasks
-from gcal.inkal_event import InkalEvent
-from gcal.inkal_task import InkalTask
-from power.pi_sugar import PiSugar
+
+import requests
 from pytz import timezone
 from pytz.tzinfo import DstTzInfo
 from display_data import DisplayData
 from render.render import ChromeRenderer
-from typing import Any, List
-from typings_google_calendar_api.events import Event
-
+from power.pi_sugar import PiSugar
+from PIL import Image
 import datetime as dt
 import json
 import logging
 import sys
 import os
 
-
-from PIL import Image
-
 image_dir = '/home/pi/inkal/render'
+
 
 
 def main():
     # Basic configuration settings (user replaceable)
-    configFile = open("config.json")
-    config: Config = json.load(configFile)
-
+    with open("config.json") as configFile:
+        config = json.load(configFile)
 
     #  READ CONFIGURATION
-
-    displayTZ: DstTzInfo = timezone(
-        config["displayTZ"]
-    )  # list of timezones - print(pytz.all_timezones)
-    thresholdHours = config[
-        "thresholdHours"
-    ]  # considers events updated within last 12 hours as recently updated
-    maxEventsPerDay = config[
-        "maxEventsPerDay"
-    ]  # limits number of events to display (remainder displayed as '+X more')
-    isShutdownOnComplete = config[
-        "isShutdownOnComplete"
-    ]  # set to true to conserve power, false if in debugging mode
-    alarm_interval_minutes = config[
-        "alarm_interval_minutes"
-        ]
-    screenWidth = config[
-        "screenWidth"
-    ]  # Width of E-Ink display. Default is landscape. Need to rotate image to fit.
-    screenHeight = config[
-        "screenHeight"
-    ]  # Height of E-Ink display. Default is landscape. Need to rotate image to fit.
-    imageWidth = config[
-        "imageWidth"
-    ]  # Width of image to be generated for display.
-    imageHeight = config[
-        "imageHeight"
-    ]  # Height of image to be generated for display.
-    rotateAngle = config[
-        "rotateAngle"
-    ]  # If image is rendered in portrait orientation, angle to rotate to fit screen
-    # calendars: List[str] = config["calendars"]  # Google calendar ids
-    accounts: List[Any] = config["accounts"]  # Google accounts to authenticate
+    displayTZ: DstTzInfo = timezone(config["displayTZ"])
+    thresholdHours = config["thresholdHours"]
+    maxEventsPerDay = config["maxEventsPerDay"]
+    isShutdownOnComplete = config["isShutdownOnComplete"]
+    alarm_interval_minutes = config["alarm_interval_minutes"]
+    screenWidth = config["screenWidth"]
+    screenHeight = config["screenHeight"]
+    imageWidth = config["imageWidth"]
+    imageHeight = config["imageHeight"]
+    rotateAngle = config["rotateAngle"]
 
     # Create and configure logger
     logging.basicConfig(
@@ -85,63 +52,31 @@ def main():
         filemode="a",
     )
     logger = logging.getLogger("maginkcal")
-    logger.addHandler(logging.StreamHandler(sys.stdout))  # print logger to stdout
+    logger.addHandler(logging.StreamHandler(sys.stdout))
     logger.setLevel(logging.INFO)
     logger.info("Starting daily calendar update")
 
-    # try:
-    #     sync_time(logger, displayTZ)
-
-    # except Exception as e:
-    #     logger.error(e)
-
     logger.info(msg="Time synchronised")
-
 
     try:
         currDatetime = dt.datetime.now(displayTZ)
-
         currDate = currDatetime.date()
-        calStartDate = currDate - dt.timedelta(
-            days=(currDate.weekday() % 7)
-        )
+        calStartDate = currDate - dt.timedelta(days=(currDate.weekday() % 7))
         calEndDate = calStartDate + dt.timedelta(days=(4 * 7 - 1))
-        calStartDatetime = displayTZ.localize(
-            dt.datetime.combine(calStartDate, dt.datetime.min.time())
-        )
-        calEndDatetime = displayTZ.localize(
-            dt.datetime.combine(calEndDate, dt.datetime.max.time())
-        )
 
         start: dt.datetime = dt.datetime.now()
 
-        tasks: List[InkalTask] = []
-        events: List[InkalEvent] = []
-        for account in accounts:
-            calendars: List[Calendar] = config["accounts"][account]["calendars"]
-            google_auth = GoogleAuth()
-            calendar_service, task_service = google_auth.authenticate(account)
-            google_calendar = GoogleCalendar(calendar_service)
-            google_tasks = GoogleTasks(task_service)
+        # Fetch events and tasks from HTTP endpoint
+        url = "https://script.google.com/macros/s/AKfycbxdEmpN73rq-hAZRyB8GNnwpgbA339C8qdmE53B_27HqSKGc1JHfCbcIay4GPYWk5k/exec"
+        params = {"key": "test"}
+        logger.info(f"Fetching events and tasks from {url}")
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        data = response.json()
+        logger.info("Events and tasks fetched successfully")
 
-            account_tasks: List[InkalTask] = google_tasks.retrieve_tasks(
-                calStartDatetime - dt.timedelta(weeks=3),
-                calEndDatetime,
-                displayTZ,
-                thresholdHours,
-            )
-
-            account_events: List[Event] = google_calendar.retrieve_events(
-                calendars=calendars,
-                startDatetime=calStartDatetime,
-                endDatetime=calEndDatetime,
-                localTZ=displayTZ,
-                thresholdHours=thresholdHours,
-            )
-            logger.info(msg="Calendar events retrieved in " + str(dt.datetime.now() - start))
-
-            tasks.extend(account_tasks)
-            events.extend(account_events)
+        events = data.get("calendars", [])
+        tasks = data.get("tasks", [])
 
         render_data: DisplayData = {
             "calStartDate": calStartDate,
@@ -150,7 +85,7 @@ def main():
             "maxEventsPerDay": maxEventsPerDay,
             "today": currDate,
             "tasks": tasks
-        } 
+        }
 
         renderer = ChromeRenderer(imageWidth, imageHeight, rotateAngle)
         black_image, red_image = renderer.render(render_data)
@@ -168,8 +103,7 @@ def main():
     red_image = Image.open(red_image_path)
     black_image = Image.open(black_image_path)
     # if currDate.weekday() == 0:
-    #     # calibrate display once a week to prevent ghosting
-    #     eInkDisplay.calibrate(cycles=0)  # to calibrate in production
+    #     eInkDisplay.calibrate(cycles=0)
     eInkDisplay.display(black_image, red_image)
     # eInkDisplay.sleep()
 
@@ -177,9 +111,7 @@ def main():
     battery_level = pi_sugar.get_battery()
     logger.info("Battery level at end: {:.3f}".format(battery_level))
 
-
     logger.info("Completed daily calendar update")
-    
     logger.info(
         "Checking if configured to shutdown safely - Current hour: {}".format(
             dt.datetime.now(displayTZ).hour
